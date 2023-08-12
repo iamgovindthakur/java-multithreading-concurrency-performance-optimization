121a26a913b187336608f60ee235284c31bebd3b
Govind Kumar Thakur <iamgovindthakur@gmail.com>
2023-08-12 11:13:18 +0530

    first commit

---
+++ 11 Virtual Threads and High-Performance IO/intro-to-virtual-threads/src/VirtualThreadsDemo.java
@@ -0,0 +1,29 @@
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Michael Pogrebinsky - Top Developer Academy
+ * Introduction to Virtual Threads
+ * 
+ */
+public class VirtualThreadsDemo {
+    private static final int NUMBER_OF_VIRTUAL_THREADS = 1000;
+    public static void main(String[] args) throws InterruptedException {
+        Runnable runnable = () -> System.out.println("Inside thread: " + Thread.currentThread());
+
+        List<Thread> virtualThreads = new ArrayList<>();
+
+        for (int i = 0; i < NUMBER_OF_VIRTUAL_THREADS; i++) {
+            Thread virtualThread = Thread.ofVirtual().unstarted(runnable);
+            virtualThreads.add(virtualThread);
+        }
+
+        for (Thread virtualThread : virtualThreads) {
+            virtualThread.start();
+        }
+
+        for (Thread virtualThread : virtualThreads) {
+            virtualThread.join();
+        }
+    }
+}
